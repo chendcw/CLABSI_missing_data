04d2463b22d555dad56f256a294e51f0a64eb780
chendcw <85553943+chendcw@users.noreply.github.com>
2025-02-10 12:52:27 +0100

    Update single_clean.R

--- playground/single_clean.R
+++ playground/single_clean.R
@@ -23,8 +23,6 @@ datasets_files <- list.files(data_path_play_dyn_miss,
 train_files <- datasets_files[str_detect(datasets_files, "train")]
 test_files <- datasets_files[str_detect(datasets_files, "test")]
 
-# train_files <- train_files[1:10]
-# test_files <- test_files[1:10]
 
 # keep results
 predictions <- init_preds_BASE_DYN()
@@ -82,21 +80,12 @@ for (f in train_files){
            c("LAB_WBC_count_last","LAB_WBC_Neutrophils_last")) %>%
     select(-c("LAB_is_neutropenia"))
   
-  # data_train_bin$MS_medical_specialty_bin_drop_Cardiac <- as.factor(data_train_bin$MS_medical_specialty_bin_drop_Cardiac)
-  # data_train_bin$MS_medical_specialty_bin_drop_Traumatology <- as.factor(data_train_bin$MS_medical_specialty_bin_drop_Traumatology)
-  # data_train_bin$MS_medical_specialty_bin_drop_Pediatrics <- as.factor(data_train_bin$MS_medical_specialty_bin_drop_Pediatrics)
-  # data_train$LAB_is_neutropenia <- as.factor(data_train$LAB_is_neutropenia)
+  
   data_train$ADM_admission_source_binary_all_Home <- as.factor(data_train$ADM_admission_source_binary_all_Home)
-  # data_train$MS_medical_specialty <- as.factor(data_train$MS_medical_specialty)
   data_train$MS_medical_specialty_bin_drop_Cardiac <- as.factor(data_train$MS_medical_specialty_bin_drop_Cardiac)
   data_train$MS_medical_specialty_bin_drop_Traumatology <- as.factor(data_train$MS_medical_specialty_bin_drop_Traumatology)
   data_train$MS_medical_specialty_bin_drop_Pediatrics <- as.factor(data_train$MS_medical_specialty_bin_drop_Pediatrics)
   
-  
-  # data_train$CAT_lumens_CVC <- as.factor(data_train$CAT_lumens_CVC)
-  # data_train$CAT_lumens_Tunneled_CVC <- as.factor(data_train$CAT_lumens_Tunneled_CVC)
-  # data_train$CAT_lumens_PICC <- as.factor(data_train$CAT_lumens_PICC)
-  # data_train$CARE_NEU_GCS_score_last <- as.factor(data_train$CARE_NEU_GCS_score_last)
   
   data_test <- data_test %>% 
     filter(LM <= 30) %>% 
@@ -105,20 +94,12 @@ for (f in train_files){
            c("LAB_WBC_count_last","LAB_WBC_Neutrophils_last")) %>%
     select(-c("LAB_is_neutropenia"))
   
-  # data_test_bin$MS_medical_specialty_bin_drop_Cardiac <- as.factor(data_test_bin$MS_medical_specialty_bin_drop_Cardiac)
-  # data_test_bin$MS_medical_specialty_bin_drop_Traumatology <- as.factor(data_test_bin$MS_medical_specialty_bin_drop_Traumatology)
-  # data_test_bin$MS_medical_specialty_bin_drop_Pediatrics <- as.factor(data_test_bin$MS_medical_specialty_bin_drop_Pediatrics)
-  # data_test$LAB_is_neutropenia <- as.factor(data_test$LAB_is_neutropenia)
+  
   data_test$ADM_admission_source_binary_all_Home <- as.factor(data_test$ADM_admission_source_binary_all_Home)
-  # data_test$MS_medical_specialty <- as.factor(data_test$MS_medical_specialty)
   data_test$MS_medical_specialty_bin_drop_Cardiac <- as.factor(data_test$MS_medical_specialty_bin_drop_Cardiac)
   data_test$MS_medical_specialty_bin_drop_Traumatology <- as.factor(data_test$MS_medical_specialty_bin_drop_Traumatology)
   data_test$MS_medical_specialty_bin_drop_Pediatrics <- as.factor(data_test$MS_medical_specialty_bin_drop_Pediatrics)
   
-  # data_test$CAT_lumens_CVC <- as.factor(data_test$CAT_lumens_CVC)
-  # data_test$CAT_lumens_Tunneled_CVC <- as.factor(data_test$CAT_lumens_Tunneled_CVC)
-  # data_test$CAT_lumens_PICC <- as.factor(data_test$CAT_lumens_PICC)
-  # data_test$CARE_NEU_GCS_score_last <- as.factor(data_test$CARE_NEU_GCS_score_last)
   
   set.seed(2024)
   start_time_imputation <- Sys.time()
@@ -127,14 +108,8 @@ for (f in train_files){
   predMat[c("eventtime", "type", "functioneelDossierNr"), ] <- 0
   predMat[,c("eventtime", "type", "functioneelDossierNr")] <- 0
   imputeMethod <- make.method(data_train)
-  # polyreg_vars <- c("CAT_lumens_CVC", "CAT_lumens_Tunneled_CVC", "CAT_lumens_PICC", "CARE_NEU_GCS_score_last", "MS_medical_specialty")
-  # logreg_vars <- c("ADM_admission_source_binary_all_Home")
   logreg_vars <- c("ADM_admission_source_binary_all_Home", "MS_medical_specialty_bin_drop_Cardiac", "MS_medical_specialty_bin_drop_Traumatology", "MS_medical_specialty_bin_drop_Pediatrics")
-  
-  # imputeMethod[polyreg_vars] <- "polyreg"     # polyreg regression for count variables
   imputeMethod[logreg_vars] <- "logreg"  # Logistic regression for binary variables
-  # imputeMethod[linear_vars] <- "pmm" 
-  ## do not allow Y in imputation model 
   MI <- mice(data_train, m = 1, predictorMatrix = predMat, method = imputeMethod, maxit = 1, print = F) 
   
   
@@ -157,12 +132,6 @@ for (f in train_files){
              CAT_lumens_Port_a_cath +
              CAT_lumens_PICC) 
   
-  # data_train_imputed <- data_train_imputed %>%
-  #   mutate(CAT_lumens_Total = as.numeric(as.character(CAT_lumens_CVC)) + 
-  #            as.numeric(as.character(CAT_lumens_Tunneled_CVC)) +
-  #            as.numeric(as.character(CAT_lumens_Dialysis_CVC)) +
-  #            as.numeric(as.character(CAT_lumens_Port_a_cath)) +
-  #            as.numeric(as.character(CAT_lumens_PICC))) 
   
   data_train_imputed <- data_train_imputed %>% 
     mutate(LAB_is_neutropenia = as.numeric(LAB_WBC_count_last * 1000 < 500 |
@@ -186,27 +155,12 @@ for (f in train_files){
              CAT_lumens_Port_a_cath +
              CAT_lumens_PICC) 
   
-  # data_test_imputed <- data_test_imputed %>%
-  #   mutate(CAT_lumens_Total = as.numeric(as.character(CAT_lumens_CVC)) + 
-  #            as.numeric(as.character(CAT_lumens_Tunneled_CVC)) +
-  #            as.numeric(as.character(CAT_lumens_Dialysis_CVC)) +
-  #            as.numeric(as.character(CAT_lumens_Port_a_cath)) +
-  #            as.numeric(as.character(CAT_lumens_PICC))) 
   
   data_test_imputed <- data_test_imputed %>% 
     mutate(LAB_is_neutropenia = as.numeric(LAB_WBC_count_last * 1000 < 500 |
                                              LAB_WBC_Neutrophils_last * 1000 < 500)) 
   time_imputation <- as.numeric(difftime(Sys.time(), start_time_imputation, units = "secs"))
-  
-  # binarize
-  # cat_cols <- "MS_medical_specialty"
-  
-  # bin_model <- data_train_imputed %>% 
-  #   make_col_binary_drop(cat_cols, dropped_levels = list(MS_medical_specialty = "Other"))
-  # data_train_bin <- bin_model$data
-  # data_test_bin <- data_test_imputed %>% 
-  #   make_col_binary_drop(cat_cols, dropped_levels = list(MS_medical_specialty = "Other"))
-  # data_test_bin <- data_test_bin$data
+
   
   # outcome: Surv(eventtime, type)
   
@@ -228,14 +182,8 @@ for (f in train_files){
     filter(!eventtime <= LM) %>% 
     mutate(type = if_else(type == "Censored", 0, if_else(type == "CLABSI", 1, if_else(type == "Death", 2, 3))))
   
-  # data_train_bin$LAB_is_neutropenia <- as.numeric(as.character(data_train_bin$LAB_is_neutropenia))
   data_train_bin$ADM_admission_source_binary_all_Home <- as.numeric(as.character(data_train_bin$ADM_admission_source_binary_all_Home))
-  # data_train_bin$CARE_NEU_GCS_score_last <- as.numeric(as.character(data_train_bin$CARE_NEU_GCS_score_last))
-  
-  
-  # data_test_bin$LAB_is_neutropenia <- as.numeric(as.character(data_test_bin$LAB_is_neutropenia))
   data_test_bin$ADM_admission_source_binary_all_Home <- as.numeric(as.character(data_test_bin$ADM_admission_source_binary_all_Home))
-  # data_test_bin$CARE_NEU_GCS_score_last <- as.numeric(as.character(data_test_bin$CARE_NEU_GCS_score_last))
   
   # specify the formula
   form <- paste0(" ~ cluster(id) + ",paste0(predictors_col, collapse = " + "))
